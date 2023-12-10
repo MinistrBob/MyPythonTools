314db3909ffe07977b7a6ab1a91d34b6c9308d5e
Бобровский Дмитрий <ministrbob777@gmail.com>
2023-12-10 18:29:33 +0300

    Update obsidian.py

--- Obsidian/obsidian.py
+++ Obsidian/obsidian.py
@@ -7,19 +7,27 @@ from urllib.parse import unquote
 
 
 def replace_strings_in_file(file_path):
-    # Define a regular expression pattern to match the old string
-    # pattern = re.compile(r'^!\[.*?\]\((.*?)\)')
-
+    """
+    Находит в файле все внешние ссылки r"![" и заменяет в таких строках "_resources" на "attachments",
+    а ".resources" удаляет.
+    :param file_path: Путь к файлу.
+    """
     # Read the file, replace strings, and write the modified content back
     with open(file_path, 'r', encoding='utf-8') as file:
-        # lines = [re.sub(pattern, new_string, line) if line.startswith("![") else line for line in file]
-        lines = [line.replace("_resources", "attachments").replace(".resources", "") if r"![" in line else line for line in file]
+        lines = [line.replace("_resources", "attachments").replace(".resources", "")
+                 if r"![" in line else line for line in file]
 
     with open(file_path, 'w', encoding='utf-8') as file:
         file.writelines(lines)
 
 
 def replace_content_in_parentheses(input_string, replacement):
+    """
+    (вспомогательное) Заменяет содержимое внешних ссылок в строке на replacement.
+    :param input_string:
+    :param replacement:
+    :return:
+    """
     # Define a regular expression pattern to match content within parentheses
     pattern = r'\((.*?)\)'
 
@@ -29,21 +37,34 @@ def replace_content_in_parentheses(input_string, replacement):
     return result_string
 
 
-def get_path_from_link(input_string):
-    # Define a regular expression pattern to match content within parentheses
-    pattern = r'!\[.*?\]\((.*?)\)'
+def check_link(input_string: str) -> bool:
+    """
+    Внешняя ссылка на attachment должна начинаться с ./_resources
+    :param input_string: Проверяемая строка.
+    :return: boolean
+    """
+    if input_string.startswith(r"./_resources"):
+        return True
+    else:
+        return False
 
+
+def get_path_from_link(input_string: str, pattern=r'!\[.*?\]\((.*?)\)') -> str:
+    """
+    С помощью регулярного выражения возвращаю путь к файлу attachment.
+    :param input_string:
+    :param pattern:
+    :return:
+    """
     # Use re.search to find the first match
     match = re.search(pattern, input_string)
-
     # If a match is found, return the content within parentheses, else return None
     return match.group(1) if match else None
 
 
 def calculate_execution_time(func):
     """
-    Decorator to calculate the execution time of a function.
-
+    (вспомогательное) Decorator to calculate the execution time of a function.
     :param func: The function to be decorated.
     :return: The decorated function.
     """
@@ -60,28 +81,25 @@ def calculate_execution_time(func):
 
 
 @calculate_execution_time
-def count_md_files(folder_path):
+def count_md_files(folder_path) -> int:
     """
-    Traverse the file tree starting from the given folder and count the number of *.md files.
-
+    (вспомогательное) Обход файлового дерева начиная с folder_path и подсчет количества *.md файлов.
     :param folder_path: The path to the folder to start traversing from.
     :return: The count of *.md files.
     """
     md_file_count = 0
-
     # Walk through the directory tree
     for foldername, subfolders, filenames in os.walk(folder_path):
         for filename in filenames:
             # Check if the file has a .md extension
             if filename.endswith(".md"):
                 md_file_count += 1
-
     return md_file_count
 
 
 def find_long_filepath(folder_path):
     """
-    Поиск путей файлов которые длиннее 256 символов.
+    (вспомогательное) Поиск путей файлов которые длиннее 256 символов.
     """
     # Walk through the directory tree
     for foldername, subfolders, filenames in os.walk(folder_path):
@@ -93,7 +111,8 @@ def find_long_filepath(folder_path):
 
 def process_md_files(folder_path):
     """
-    Traverse the file tree starting from the given folder, find tags in *.md files, and print results.
+    (вспомогательное) Обход файлового дерева начиная с folder_path и обработка каждого *.md файла.
+    В данном случае печать списка тегов для файла.
 
     :param folder_path: The path to the folder to start traversing from.
     """
@@ -131,7 +150,7 @@ def process_md_files(folder_path):
 
 def create_tag_folders(tags, destination_path):
     """
-    Create folders corresponding to tags if they don't exist in the destination path.
+    Создать папки, соответствующие тегам, если они не существуют в пути назначения.
 
     :param tags: List of tags.
     :param destination_path: Path to the destination folder.
@@ -158,6 +177,31 @@ def create_tag_folders(tags, destination_path):
             print(f"Folder created: {folder_path}")
         # else:
         #     print(f"Folder already exists: {folder_path}")
+
+
+def delete_attach_folder(source_dir, dest_dir):
+    # Check if source_dir exists
+    if os.path.exists(source_dir) and os.path.isdir(source_dir):
+        # Check if source_dir has any files
+        files_in_source_dir = os.listdir(source_dir)
+
+        if files_in_source_dir:
+            # source_dir has files, so copy them to folder2
+            if not os.path.exists(dest_dir):
+                os.makedirs(dest_dir)
+            for file in files_in_source_dir:
+                file_path = os.path.join(source_dir, file)
+                shutil.move(file_path, dest_dir)
+
+            # Delete source_dir
+            os.rmdir(source_dir)
+            print(f"Files from {source_dir} moved to {dest_dir}, and {source_dir} deleted.")
+        else:
+            # source_dir is empty, so just delete it
+            os.rmdir(source_dir)
+            print(f"{source_dir} is empty and has been deleted.")
+    else:
+        print(f"{source_dir} does not exist or is not a directory.")
 
 
 def move_md_files(data_path, tags, destination_path, dry=True):
@@ -206,8 +250,25 @@ def move_md_files(data_path, tags, destination_path, dry=True):
                         with open(file_path, 'r', encoding='utf-8') as md_file:
                             for line in md_file:
                                 if r"![" in line:
-                                    links.append(get_path_from_link(line))
+                                    print(f"link line: {line}")
+                                    # Получаем ссылку и проверяем ее на корректность, если она корректна то добавляем в список,
+                                    # если нет, пробуем другие варианты pattern.
+                                    res = get_path_from_link(line, pattern=r'!\[.*?\]\((.*)\)')
+                                    print(f"    res1: {res}")
+                                    if res and check_link(res):
+                                        links.append(res)
+                                    else:
+                                        res = get_path_from_link(line, pattern=r'!\[\[\s*(.*?)\s*\]\]')
+                                        print(f"    res2: {res}")
+                                        if res and check_link(res):
+                                            links.append(res)
+                                        else:
+                                            raise ValueError(f"Ни один из паттернов не подошёл, нужно искать другие")
+                                    print("-" * 60)
                             print(f"    Links: {links}")
+                            for link in links:
+                                print(link)
+                            print("-" * 60)
                         # Если у документа нет ссылок тогда мы его просто переносим в папку destination_path/tag
                         # иначе переносим ещё и связанные с документом вложения.
                         # Удалить символ # из имени тега.
@@ -239,11 +300,13 @@ def move_md_files(data_path, tags, destination_path, dry=True):
                                 # Создать папку для вложений конкретной заметки в папке destination_path/tag/attachments
                                 if not dry:
                                     print("Create note attache folder: " + dest_tag_dir)
-                                    create_tag_folders([dest_attach_dir_name], os.path.join(dest_tag_dir, "attachments"))
+                                    create_tag_folders([dest_attach_dir_name],
+                                                       os.path.join(dest_tag_dir, "attachments"))
                                 # Перенести вложение в папку destination_path/tag/attachments/folder_name
                                 ## Исходный путь к файлу вложения (dest_attach_file_name сюда подходит,
                                 # потому что исходное и целевое имя файла одно и тоже).
-                                source_attach_file = os.path.join(data_path, "_resources", link[-2], dest_attach_file_name)
+                                source_attach_file = os.path.join(data_path, "_resources", link[-2],
+                                                                  dest_attach_file_name)
                                 ## Целевой путь к целевой папке вложений и файлу вложения.
                                 dest_attach_dir = os.path.join(dest_tag_dir, "attachments", dest_attach_dir_name)
                                 dest_attach_file = os.path.join(dest_attach_dir, dest_attach_file_name)
@@ -256,13 +319,20 @@ def move_md_files(data_path, tags, destination_path, dry=True):
                                         shutil.move(source_attach_file, dest_attach_file)
                                     except FileNotFoundError:
                                         print(f"File not found: {source_attach_file}")
+                                # Копировать оставшиеся вложения в папку destination_path/tag/attachments/folder_name
+                                # и удалять папку.
+                                if not dry:
+                                    source_attach_dir = os.path.join(data_path, "_resources", link[-2])
+                                    print(f"source_attach_dir={source_attach_dir}")
+                                    delete_attach_folder(source_attach_dir, dest_attach_dir)
                             # Перемещение заметки
                             if not dry:
                                 replace_strings_in_file(file_path)
                             print(f"    Moving file: {file_path} to {dest_tag_dir}")
                             if not dry:
                                 shutil.move(file_path, dest_tag_dir)
-                            # TODO копировать оставшиеся вложения в папку destination_path/tag/attachments/folder_name и удалять папку.
+
+                        print(f"\n{'=' * 60}\n")
 
 
 if __name__ == '__main__':
@@ -288,5 +358,5 @@ if __name__ == '__main__':
     # print(test_path)
 
     destination_path = r"d:\YandexDisk\ObsidianVault\MainVault\АДМИНИСТРИРОВАНИЕ"
-    # move_md_files(DATA_PATH, ["#Cisco"], destination_path)
-    move_md_files(DATA_PATH, ["#Cisco"], destination_path, dry=False)
+    # move_md_files(DATA_PATH, ["#SharePoint"], destination_path)
+    move_md_files(DATA_PATH, ["#SharePoint"], destination_path, dry=False)
