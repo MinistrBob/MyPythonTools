8f09f8b19612c7da90d014b1dcc8a4511ec4ec5a
Бобровский Дмитрий <ministrbob777@gmail.com>
2023-12-19 18:36:40 +0300

    Update obsidian.py

--- Obsidian/obsidian.py
+++ Obsidian/obsidian.py
@@ -166,34 +166,55 @@ def work_with_links_in_md_files(file_path, relative_path):
                 print(f"  {i}")
 
 
-def get_tags_from_md(file_path, relative_path):
+def get_tags_from_md(folder_path):
     """
     (вспомогательное) Находит в файле md теги и печатает их. Или выводит сообщение об отсутствии тегов.
+    Можно изменять конечную часть процедуры, чтобы получать разный вывод.
     :param file_path:
     :param relative_path:
     :return:
     """
-    tags_list = []
-    with open(file_path, 'r', encoding='utf-8') as file:
-        for line in file:
-            if line.startswith("tags:"):
-                # Extract the tags using ast.literal_eval to safely parse the string
-                tags_str = line.replace("tags:", "").strip()
-                try:
-                    tags_list = ast.literal_eval(tags_str)
-                except (SyntaxError, ValueError):
-                    print(f"    Error parsing tags in file: {relative_path}")
-                break
-        if not tags_list:
-            # Print relative path
-            print(f"File: {relative_path}")
-            print(f"    No tags found in file: {relative_path}")
-        else:
-            # Print relative path
-            # print(f"File: {relative_path}")
-            # Print the list of tags
-            # print(f"    Tags: {tags_list}")
-            pass
+    all_tags =[]
+    # Walk through the directory tree
+    for foldername, subfolders, filenames in os.walk(folder_path):
+        for filename in filenames:
+            # Check if the file has a .md extension
+            if filename.endswith(".md"):
+                file_path = os.path.join(foldername, filename)
+                relative_path = os.path.relpath(file_path, folder_path)
+                # print(f"file_path: {file_path}")
+                # print(f"relative_path: {relative_path}")
+                tags_list = []
+                with open(file_path, 'r', encoding='utf-8') as file:
+                    for line in file:
+                        if line.startswith("tags:"):
+                            # Extract the tags using ast.literal_eval to safely parse the string
+                            tags_str = line.replace("tags:", "").strip()
+                            try:
+                                tags_list = ast.literal_eval(tags_str)
+                            except (SyntaxError, ValueError):
+                                print(f"    Error parsing tags in file: {relative_path}")
+                            break
+                    if not tags_list:  # НЕТ ТЕГОВ
+                        # Print relative path
+                        # print(f"File: {relative_path}")
+                        # print(f"    No tags found in file: {relative_path}")
+                        pass
+                    else:  # ЕСТЬ ТЕГИ
+                        # Обрабатываемый файл
+                        # print(f"File: {relative_path}")
+                        # Список тегов
+                        # print(f"    Tags: {tags_list}")
+                        # Только список тегов
+                        # print(tags_list)
+                        # Собрать список всех тегов во всех файлах
+                        all_tags.extend(tags_list)
+                        # pass
+    if all_tags:
+        set_tags = set(all_tags)  # Удаляем дубли
+        sorted_all_tags = sorted(set_tags)  # set не поддерживает сортировку
+        for tag in sorted_all_tags:
+            print(tag)
 
 
 def create_tag_folders(tags, destination_path):
@@ -211,7 +232,7 @@ def create_tag_folders(tags, destination_path):
     for tag in tags:
         # Remove the '#' symbol if present at the beginning
         if tag.startswith('#'):
-            folder_name = tag.lstrip('#')
+            folder_name = tag.lstrip('#').replace('-', ' ')
         else:
             folder_name = tag
 
@@ -228,31 +249,46 @@ def create_tag_folders(tags, destination_path):
 
 
 def delete_attach_folder(source_dir, dest_dir):
+    """
+    После переноса вложений attache папка удаляется.
+    :param source_dir:
+    :param dest_dir:
+    :return:
+    """
+    print(f"  delete_attach_folder for {source_dir} and {dest_dir}")
     # Check if source_dir exists
     if os.path.exists(source_dir) and os.path.isdir(source_dir):
         # Check if source_dir has any files
         files_in_source_dir = os.listdir(source_dir)
 
         if files_in_source_dir:
+            print(f"  В папке ещё есть файлы")
             # source_dir has files, so copy them to folder2
             if not os.path.exists(dest_dir):
                 os.makedirs(dest_dir)
             for file in files_in_source_dir:
                 file_path = os.path.join(source_dir, file)
+                print(f"  Move attach {file_path} to {dest_dir}")
                 shutil.move(file_path, dest_dir)
 
             # Delete source_dir
+            print(f"  Delete dir {source_dir}")
             os.rmdir(source_dir)
-            print(f"Files from {source_dir} moved to {dest_dir}, and {source_dir} deleted.")
         else:
             # source_dir is empty, so just delete it
+            print(f"  Delete EMPTY dir {source_dir}")
             os.rmdir(source_dir)
-            print(f"{source_dir} is empty and has been deleted.")
     else:
         print(f"{source_dir} does not exist or is not a directory.")
 
 
 def remove_parentheses(data_path, dry=True):
+    """
+    Удаление скобок в названиях md файлов, в именах ресурсных подпапок и в линках в заметке.
+    :param data_path:
+    :param dry:
+    :return:
+    """
     for foldername, subfolders, filenames in os.walk(data_path):
         for filename in filenames:
             if filename.endswith('.md') and ('(' in filename or ')' in filename):
@@ -330,7 +366,7 @@ def move_md_files(data_path: str, tags: List[str], destination_path: str, dry: b
                             try:
                                 tags_list = ast.literal_eval(tags_str)
                             except (SyntaxError, ValueError):
-                                print(f"    Error parsing tags in file: {relative_path}")
+                                print(f"Error parsing tags in file: {relative_path}")
                             break
                 # Работаем только с файлами с тегами, остальные потом обработаю.
                 if tags_list:
@@ -339,32 +375,32 @@ def move_md_files(data_path: str, tags: List[str], destination_path: str, dry: b
                     first_matching_tag = next((item for item in tags if item in tags_list), None)
                     if first_matching_tag is not None:
                         # Файл соответствует списку тегов tags
-                        print(f"Tag: {first_matching_tag} in file: {file_path}")
+                        print(f"  Tag: {first_matching_tag} in file: {file_path}")
                         # Собираем все ссылки из документа
                         links = []
                         with open(file_path, 'r', encoding='utf-8') as md_file:
                             for line in md_file:
                                 if r"![" in line:
-                                    print(f"link line: {line}")
+                                    print(f"  link line: {line}")
                                     # Получаем ссылку и проверяем ее на корректность, если она корректна то добавляем в список,
                                     # если нет, пробуем другие варианты pattern.
                                     # Вариант ссылки в круглых скобках:
                                     # [![](./_resources/Глобальные_платформы_для_Фрилансеров_-The_Most_Popular_Platforms.1.resources/unknown_filename.png)](https://blog.payoneer.com/ru/author/richardcl/)
                                     res = get_path_from_link(line, pattern=r'!\[.*?\]\((.*?)\)')
-                                    print(f"    res1: {res}")
+                                    print(f"  res1: {res}")
                                     if res and check_link(res):
                                         links.append(res)
                                     else:
                                         # Вариант ссылки в квадратных скобках:
                                         # [![[./_resources/Совместная_работа_с_документами_SharePoint_2016,_Office_Online_и_все-все-все._Часть_1._Что_это__Хабр.resources/embedded.83.svg]]](https://habr.com/ru/post/310396/#comment_9817182)
                                         res = get_path_from_link(line, pattern=r'!\[\[\s*(.*?)\s*\]\]')
-                                        print(f"    res2: {res}")
+                                        print(f"  res2: {res}")
                                         if res and check_link(res):
                                             links.append(res)
                                         else:
                                             raise ValueError(f"Ни один из паттернов не подошёл, нужно искать другие")
                                     print("-" * 60)
-                            print(f"    Links: {links}")
+                            print(f"  Links: {links}")
                             for link in links:
                                 print(link)
                             print("-" * 60)
@@ -380,28 +416,28 @@ def move_md_files(data_path: str, tags: List[str], destination_path: str, dry: b
                             dest_tag_dir = os.path.join(destination_path, first_matching_tag)
                         if not links:
                             # Перемещение заметки
-                            print(f"    Moving file: {file_path} to {dest_tag_dir}")
+                            print(f"  Moving file: {file_path} to {dest_tag_dir}")
                             if not dry:
                                 shutil.move(file_path, dest_tag_dir)
                         else:
-                            print("Create attachments folder: " + dest_tag_dir)
+                            print("  Create attachments folder: " + dest_tag_dir)
                             if not dry:
                                 create_tag_folders(["attachments"], dest_tag_dir)
                             # Перемещение вложений
                             for link in links:
                                 # Выделить из ссылки имя папки и имя файла
                                 link = link.split("/")
-                                print(f"Link parts: {link}")
+                                print(f"  Link parts: {link}")
                                 # Так будет называться папка вложений на новом месте
                                 dest_attach_dir_name = link[-2].replace(".resources", "")
                                 # Имя файла вложения. В некоторых ссылках имена файлов идут как URL-encoded string,
                                 # поэтом производиться декодирование имени файла.
                                 dest_attach_file_name = unquote(link[-1], encoding='utf-8')
-                                print(f"dest_attach_dir_name={dest_attach_dir_name}")
-                                print(f"dest_attach_file_name={dest_attach_file_name}")
+                                print(f"  dest_attach_dir_name={dest_attach_dir_name}")
+                                print(f"  dest_attach_file_name={dest_attach_file_name}")
                                 # Создать папку для вложений конкретной заметки в папке destination_path/tag/attachments
                                 if not dry:
-                                    print("Create note attache folder: " + dest_tag_dir)
+                                    print("  Create note attache folder: " + dest_tag_dir)
                                     create_tag_folders([dest_attach_dir_name],
                                                        os.path.join(dest_tag_dir, "attachments"))
                                 # Перенести вложение в папку destination_path/tag/attachments/folder_name
@@ -412,10 +448,10 @@ def move_md_files(data_path: str, tags: List[str], destination_path: str, dry: b
                                 ## Целевой путь к целевой папке вложений и файлу вложения.
                                 dest_attach_dir = os.path.join(dest_tag_dir, "attachments", dest_attach_dir_name)
                                 dest_attach_file = os.path.join(dest_attach_dir, dest_attach_file_name)
-                                print(f"source_attach_file={source_attach_file}")
-                                print(f"dest_attach_dir={dest_attach_dir}")
-                                print(f"dest_attach_file={dest_attach_file}")
-                                print(f"    Moving attach: {source_attach_file} to {dest_attach_file}")
+                                print(f"  source_attach_file={source_attach_file}")
+                                print(f"  dest_attach_dir={dest_attach_dir}")
+                                print(f"  dest_attach_file={dest_attach_file}")
+                                print(f"  Moving attach: {source_attach_file} to {dest_attach_file}")
                                 if not dry:
                                     try:
                                         shutil.move(source_attach_file, dest_attach_file)
@@ -425,12 +461,12 @@ def move_md_files(data_path: str, tags: List[str], destination_path: str, dry: b
                                 # и удалять папку.
                                 if not dry:
                                     source_attach_dir = os.path.join(data_path, "_resources", link[-2])
-                                    print(f"source_attach_dir={source_attach_dir}")
+                                    print(f"  source_attach_dir={source_attach_dir}")
                                     delete_attach_folder(source_attach_dir, dest_attach_dir)
                             # Перемещение заметки
                             if not dry:
                                 replace_strings_in_file(file_path)
-                            print(f"    Moving file: {file_path} to {dest_tag_dir}")
+                            print(f"  Move md file: {file_path} to {dest_tag_dir}")
                             if not dry:
                                 shutil.move(file_path, dest_tag_dir)
                         count+=1
@@ -444,8 +480,8 @@ if __name__ == '__main__':
     # execution_time, result= count_md_files(DATA_PATH)
     # print(f"({execution_time} sec.) Number of *.md files: {result}")  # Number of *.md files: 7572
 
-    # (в текущем примере) выводит файлы md не имеющих тегов.
-    # process_md_files(DATA_PATH, get_tags_from_md)
+    # Вывод тегов в разных видах или md файлов без тегов.
+    # get_tags_from_md(DATA_PATH)
 
     # (в текущем примере) выводит список ссылок "![" из md файла.
     # process_md_files(DATA_PATH, work_with_links_in_md_files)
@@ -468,6 +504,21 @@ if __name__ == '__main__':
     # remove_parentheses(DATA_PATH, dry=True)
     # remove_parentheses(DATA_PATH, dry=False)
 
-    destination_path = r"d:\YandexDisk\ObsidianVault\MainVault\ДЕНЕЖНЫЙ ПОТОК"
-    # move_md_files(DATA_PATH, ["#@@@-ИЗУЧЕНИЕ"], destination_path)
-    move_md_files(DATA_PATH, ["#Криптовалюта"], destination_path, dry=False)
+    # ПУТИ
+    # destination_path = r"d:\YandexDisk\ObsidianVault\MainVault\ПРОГРАММНОЕ ОБЕСПЕЧЕНИЕ"
+    destination_path = r"d:\YandexDisk\ObsidianVault\MainVault\АДМИНИСТРИРОВАНИЕ"
+    tag_list = ["#Виртуализация-Virtualization", "#Vagrant", "#Virtualbox", "#VMware", "#VMware-PowerCLI",
+                "#VMware-ZZZ", "#VMware-administration", "#VMware-automation", "#VMware-backup", "#VMware-cluster",
+                "#VMware-errors", "#VMware-hardware", "#VMware-install", "#VMware-monitoring", "#VMware-tools",
+                "#VMware-vCenter", "#Изучение-VMWare", "#ovftool"]
+    # move_md_files(DATA_PATH, tag_list, destination_path)
+    move_md_files(DATA_PATH, tag_list, destination_path, dry=False)
+
+    # Перенос шпаргалок
+    """
+    DATA_PATH = r"d:\YandexDisk\ObsidianVault\MainVault\_Evernote_\шпаркгалка"
+    destination_path = r"d:\YandexDisk\ObsidianVault\MainVault"
+    tag_list = ["#@@@-ШПАРГАЛКИ"]
+    move_md_files(DATA_PATH, tag_list, destination_path)
+    move_md_files(DATA_PATH, tag_list, destination_path, dry=False)
+    """
