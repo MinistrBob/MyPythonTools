199148c3959016c0151ca24238faf7d6a2d58ae5
Бобровский Дмитрий <ministrbob777@gmail.com>
2024-05-04 13:07:06 +0300

    Логирование в консоль в цвете

--- Logging/custom_logger.py
+++ Logging/custom_logger.py
@@ -6,6 +6,44 @@ import traceback
 from datetime import datetime
 
 
+class CustomFormatter(logging.Formatter):
+    """Logging colored formatter, adapted from https://stackoverflow.com/a/56944256/3638629
+    The color coding \x1b[38;21m you're seeing is an ANSI escape code for setting text color in terminal output. In this case, it sets the text color to grey.
+    Here's a breakdown of the code:
+    \x1b: Escape character indicating the start of an escape sequence.
+    [38;21m: ANSI escape sequence for setting the foreground (text) color.
+    In more detail:
+    38: Select the foreground color.
+    21: Set the color to grey.
+
+    """
+    # Здесь белый текст на "черном" фоне, но реально здесь 0m это просто фон (default), он в консоли обычно не чёрный,
+    # а сероватый. ChatGPT выдал мне для белого текста на чёрном фоне такой код: white_on_black = '\x1b[97;40m'
+    white_on_black = '\x1b[97;0m'
+    grey = '\x1b[38;21m'
+    blue = '\x1b[38;5;39m'
+    yellow = '\x1b[38;5;226m'
+    red = '\x1b[38;5;196m'
+    bold_red = '\x1b[31;1m'
+    reset = '\x1b[0m'
+
+    def __init__(self, fmt):
+        super().__init__()
+        self.fmt = fmt
+        self.FORMATS = {
+            logging.DEBUG: self.grey + self.fmt + self.reset,
+            logging.INFO: self.white_on_black + self.fmt + self.reset,
+            logging.WARNING: self.yellow + self.fmt + self.reset,
+            logging.ERROR: self.red + self.fmt + self.reset,
+            logging.CRITICAL: self.bold_red + self.fmt + self.reset
+        }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+
 def get_logger(settings):
     """
     The procedure creates a logger based on either program_file or log_file.
@@ -23,9 +61,10 @@ def get_logger(settings):
     # log_formatter = logging.Formatter('%(asctime)s|%(levelname)s|%(name)s|%(process)d:%(thread)d - %(message)s')
     custom_logger = logging.getLogger("teleservice-logger")
     custom_logger.setLevel(log_level)
-    stdout_log_formatter = logging.Formatter('%(asctime)s|%(levelname)-5s|%(funcName)s| %(message)s')
     stdout_handler = logging.StreamHandler(sys.stdout)
-    stdout_handler.setFormatter(stdout_log_formatter)
+    fmt = r'%(asctime)s|%(levelname)-5s|%(funcName)s| %(message)s'
+    # stdout_handler.setFormatter(logging.Formatter(fmt))
+    stdout_handler.setFormatter(CustomFormatter(fmt))
     custom_logger.addHandler(stdout_handler)
     if settings.log_to_file:
         # Log file example /home/<user>/script/log/app.log
@@ -61,5 +100,5 @@ def get_logger(settings):
         file_handler.setFormatter(file_log_formatter)
         custom_logger.addHandler(file_handler)
     # print(custom_logger.handlers[0].baseFilename)
- 
+
     return custom_logger
--- Logging/main.py
+++ Logging/main.py
@@ -6,7 +6,11 @@ log = custom_logger.get_logger(appset)
 
 
 def main():
-    log.info('Hello, World!')
+    log.debug('This is debug message')
+    log.info('This is info message')
+    log.warning('This is warning message')
+    log.error('This is error message')
+    log.critical('This is critical message')
 
 
 if __name__ == '__main__':
